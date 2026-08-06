6154cc5928f65ca684f6cee2eee4ee064932b062
pablocerrutti <pablocerrutti@gmail.com>
2026-08-06 10:23:08 -0300

    Update mapa.js

--- modulos/movilidad/mapa.js
+++ modulos/movilidad/mapa.js
@@ -30,3 +30,84 @@ async function iniciarPagina(){
     await cargarElementos();
 
 }
+//--------------------------------------------------
+// SESION
+//--------------------------------------------------
+
+function comprobarSesion(){
+
+    let usuario = null;
+
+    try{
+
+        usuario = JSON.parse(
+            localStorage.getItem("usuarioActual")
+        );
+
+    }catch(e){}
+
+    if(!usuario){
+
+        location.href="../../index.html";
+        return false;
+
+    }
+
+    document.getElementById(
+        "usuarioActual"
+    ).textContent =
+        usuario.nombre ||
+        usuario.usuario;
+
+    return true;
+
+}
+//--------------------------------------------------
+// MAPA
+//--------------------------------------------------
+
+function iniciarMapa(){
+
+    mapa = L.map("map",{
+
+        zoomControl:true
+
+    }).setView(
+
+        [-34.0997,-56.2140],
+
+        15
+
+    );
+
+    L.tileLayer(
+
+        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+
+        {
+
+            attribution:"© OpenStreetMap",
+
+            maxZoom:20
+
+        }
+
+    ).addTo(mapa);
+
+    capaMarcadores = L.layerGroup().addTo(mapa);
+
+    mapa.on(
+
+        "click",
+
+        seleccionarUbicacion
+
+    );
+
+    setTimeout(()=>{
+
+        mapa.invalidateSize();
+
+    },300);
+
+}
